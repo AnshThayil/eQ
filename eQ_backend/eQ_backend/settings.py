99a6d76015b23c85c4271dd68660a1d063b8badc
Ansh Thayil <ansh.thayil@DLT260013.local>
2026-05-03 16:19:18 +0530

    update URL

--- eQ_backend/eQ_backend/settings.py
+++ eQ_backend/eQ_backend/settings.py
@@ -53,7 +53,7 @@ DEBUG = True
 
 # Add your ngrok URL here when using ngrok tunnel
 # Example: ALLOWED_HOSTS = ['xxxx-xxxx-xxxx.ngrok-free.app', 'localhost', '127.0.0.1']
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '64.227.142.137', 'eq-backend.anshthayil.in']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '64.227.142.137', 'eq-backend.jointadventure.in']
 
 
 # Application definition
